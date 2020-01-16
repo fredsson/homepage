b47b0eaf8020ce9d048497adcd1d2a67b48b7380
Fredrik Andersson <fredrik.ks.andersson@gmail.com>
2020-01-16 21:41:30 +0100

    added navigation

--- src/app/layout/nav/nav.component.ts
+++ src/app/layout/nav/nav.component.ts
@@ -7,6 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
+  public socialLinks = [
+    {
+      href: "https://github.com/fredsson",
+      img: {src: "assets/img/github.svg", alt: "github", css: "social__icon"},
+      css: "social__github"
+    },
+    {
+      href: "https://www.linkedin.com/in/fredrik-andersson-81440966/",
+      img: { src: "assets/img/linkedin.svg", alt: "linkedin", css: "social__icon "},
+      css: "social__linkedin"
+    },
+    {
+      href: "https://twitter.com/fredssoncode",
+      img: { src: "assets/img/twitter.svg", alt: "twitter", css: "social__icon "},
+      css: "social__twitter"
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
