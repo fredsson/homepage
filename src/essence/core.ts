360fbe8c690071228c73c5742fa131f6113dbc1e
Fredrik Andersson <fredrik.ks.andersson@gmail.com>
2023-06-07 22:54:35 +0200

    now possible to type state on ViewModel

--- src/essence/core.ts
+++ src/essence/core.ts
@@ -5,10 +5,11 @@ export function isDefined<T>(value: T | undefined | null): value is T {
 export interface ViewModelConfig {
   inputs: {selector: string, change: (ev: any) => void}[],
   bindings: {selector: string, propertyName: string}[]
-  events: {selector: string, event: string, callback: () => void}[]
+  events: {selector: string, event: string, callback: (ev: MouseEvent) => void}[]
 }
 
-export interface ViewModel {
+export interface ViewModel<T = any> {
+  state?: T
   config(): ViewModelConfig;
   canActivate?(): Promise<boolean>;
   init(): void;
