075374863b3653b8574e64582117ed431e8d01e8
Fredrik Andersson <fredrik.andersson@consid.se>
2023-08-03 14:41:29 +0200

    change style of homepage

--- src/essence/essence.ts
+++ src/essence/essence.ts
@@ -3,12 +3,17 @@ import { ComponentManager } from './framework/component-manager';
 import { Router } from './framework/router';
 import { PlatformService } from './framework/platform-service';
 
+import {registerEssenceSvg} from './tools/essence-svg';
+
+
 const di = new DependencyInjector();
 
 // TODO: not nice that these needs to be in specific order:
 di.addClass(PlatformService);
 di.addClass(ComponentManager);
 di.addClass(Router);
+
+registerEssenceSvg();
 
 export {
   ComponentManager,
